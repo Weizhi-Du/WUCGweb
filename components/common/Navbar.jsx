fd84fd08ecc1e4e57d00d427843ff28a0108900a
Dijkstra <58095958+TJor-L@users.noreply.github.com>
2024-09-03 21:13:47 -0500

    Update Navbar.jsx

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -84,7 +84,7 @@ function Navbar() {
             href="/page-contact"
             className="butn butn-sm butn-bg main-colorbg radius-5"
           >
-            <span className="text">Let&apos;s contact</span>
+            <span className="text">Let&apos;s Connect</span>
           </a>
         </div>
       </div>
